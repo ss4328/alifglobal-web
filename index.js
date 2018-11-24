eb89efe75f5b1cce340d8d60667e95168e9f5208
EC2 Default User <ec2-user@ip-172-31-19-250.ec2.internal>
2018-11-24 01:24:43 +0000

    Added all the articles, products, logo, and all the graphic work

--- index.js
+++ index.js
@@ -11,6 +11,14 @@ app.get("/", function(req,res){
     res.render('index');
 });
 
+app.get("/hammer", function(req,res){
+    res.render('hammer');
+});
+
+app.get("/hammerBit", function(req,res){
+    res.render('hammerBit');
+});
+
 app.get("/home", function(req,res){
     res.render('index');
 });
@@ -19,9 +27,34 @@ app.get("/about", function(req,res){
     res.render('about');
 });
 
+app.get("/dthDrilling", function(req,res){
+    res.render('DTHDrilling');
+});
+
+app.get("/effectUnderWaterTemperatureAltitude", function(req,res){
+    res.render('effectUnderWaterTemperatureAltitude');
+});
+
 app.get("/contact", function(req,res){
     res.render('contact');
 });
+
+app.get("/hammerStorage", function(req,res){
+    res.render('hammerStorage');
+});
+
+app.get("/rotationSpeed", function(req,res){
+    res.render('rotationSpeed');
+});
+
+app.get("/totalWeightOnBitOrFeedPressure", function(req,res){
+    res.render('totalWeightOnBitOrFeedPressure');
+});
+
+app.get("/drillingWithFoamAndPolymers", function(req,res){
+    res.render('drillingWithFoamAndPolymers');
+});
+
 
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server has Started!");
